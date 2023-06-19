f4036877af146eeaa72b32412920cbb626a56a8d
cedarwarman <cedardalewarman@gmail.com>
2023-06-19 13:06:18 -0700

    Update for Calvin

--- bash/manage_slurm_script.sh
+++ bash/manage_slurm_script.sh
@@ -3,7 +3,7 @@
 # Check to see if there are any videos to process
 echo "Searching for image sequences"
 
-image_sequence_count=$(find /xdisk/rpalaniv/cedar/image_processing/source_tifs/ -maxdepth 1 -name '*run*' | wc -l)
+image_sequence_count=$(find /xdisk/rpalaniv/cedar/cv/images/source_tifs/ -maxdepth 1 -name '*run*' | wc -l)
 
 # If no image sequences, exits the script
 if [ ${image_sequence_count} = 0 ]
@@ -26,5 +26,5 @@ echo "Running Slurm script with array of ${image_sequence_count}"
 
 # I think I can just pass in the array number as an sbatch option, with the varible. Apparently the commend line options take precidence over the header options. 
 
-sbatch --array=1-${image_sequence_count} /home/u16/cedar/git/organize_and_process_microscope_images/slurm/process_image_sequences.slurm
+sbatch --array=1-${image_sequence_count} /home/u16/cedar/git/organize_and_process_microscope_images/slurm/process_image_sequences_camera_2.slurm
 
